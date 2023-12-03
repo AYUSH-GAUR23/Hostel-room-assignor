a4b191eb774bf1c091fbd665ca2a143b332b3e5a
AYUSH-GAUR23 <152532532+AYUSH-GAUR23@users.noreply.github.com>
2023-12-03 11:47:54 +0530

    hoste_room_assign.py

--- hoste_room_assign.py
+++ hoste_room_assign.py
@@ -1,22 +1,21 @@
-floors = int(input("Enter no of floors in hostel: "))
-rooms = int(input("Enter no of rooms in a floor: "))
+floors = int(input("Enter no of floors in hostel: "))  # Takes input from user about hostel details
+rooms = int(input("Enter no of rooms in a floor: "))   
 
 n = floors*(rooms*4)
-print("Student capacity:",n)
+print("Student capacity:",n)                    # calculates capacity of hostel
 
 l1 = []
 
 for i in range(n):
-    name = input("Enter name: ")
+    name = input("Enter name: ")                # Takes input as student name and create a list of it
     l1.append(name)
 
 
 l2 = []
 
-l3= ["Bed A","Bed B","Bed C","Bed D"]
+l3= ["Bed A","Bed B","Bed C","Bed D"]          # bed names in a room 
 
-for i in range(1,floors+1):
-    
+for i in range(1,floors+1):                    # A list is created which contains tuples as element containing uniqe details
     for j in range(1,rooms+1):
         for k in range(len(l3)):
             a =  "Floor no "+str(i)
@@ -25,7 +24,7 @@ for i in range(1,floors+1):
             d = (a,b,c)
             l2.append(d)
 
-dict1 = dict(zip(l1,l2))
+dict1 = dict(zip(l1,l2))                       # A dicttionary is created out of two above lists
 
 for key,value in dict1.items():
     print( key,"---->",value)
